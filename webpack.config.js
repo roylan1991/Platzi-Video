bdb5527be8ab399bb112d71be259ed76a84bdc58
Roylan Alejandro Bressler Camps <43889144+roylan1991@users.noreply.github.com>
2020-09-17 19:53:58 -0400

    Integración de Webpack con Express

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Webpack = require('webpack');
 
 module.exports = {
-    entry: './src/frontend/index.js',
+    entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true'],
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -56,6 +58,7 @@ module.exports = {
         historyApiFallback: true,
     },
     plugins: [
+        new Webpack.HotModuleReplacementPlugin(),
         new HtmlWebPackPlugin({
             template: './public/index.html',
             filename: './index.html'
